943d026e5658cbebeddc536273c8b55ec3415b4d
Serik Czarnecki <serikaczarnecki@gmail.com>
2022-11-03 20:17:49 -0400

    update
    updadt

--- src/service/firebaseinit.js
+++ src/service/firebaseinit.js
@@ -1,15 +1,30 @@
+import firebase from 'firebase/app'; // <-- This must be first
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
+
 try {
-    firebase.initializeApp({
-      apiKey: "AIzaSyCwYCjJs1ORktVIG85LVGuLmHDQKFlEJZ0",
-      authDomain: "hermes-superchat.firebaseapp.com",
-      databaseURL: "https://hermes-superchat-default-rtdb.firebaseio.com",
-      projectId: "hermes-superchat",
-      storageBucket: "hermes-superchat.appspot.com",
-      messagingSenderId: "2041104259",
-      appId: "1:2041104259:web:4164082fcdae22cc2ad8b6"
-    });
-  } catch (error) {
-    if (!/already exists/u.test(error.message)) {
-      console.error('Firebase admin initialization error', error.stack);
-    }
+  firebase.initializeApp({
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+  });
+} catch (error) {
+  if (!/already exists/u.test(error.message)) {
+    console.error('Firebase admin initialization error', error.stack);
   }
+}
+
+// Passing off firebase.auth() instead of firebase.auth
+// allows us to share the same instance of Firebase throughout
+// the entire app whenever we import it from here.
+
+export const fb = {
+  auth: firebase.auth(),
+  storage: firebase.storage(),
+  firestore: firebase.firestore(),
+};
